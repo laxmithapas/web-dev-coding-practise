afa17ce54016d1c067fc38a82e4c81e0ff05d7d5
laxmithapas <shakharthapa2004@gmail.com>
2025-07-10 16:39:44 +0530

    added solution for even-odd check in JavaScript

---
+++ 0002-even-odd-js/solution.js
@@ -0,0 +1,9 @@
+let number = prompt("Enter the number:");
+ number = Number(number);
+
+ if (number % 2 === 0 ) {
+    console.log("The number is even");
+
+ } else {
+    console.log("The number is odd");
+ }
